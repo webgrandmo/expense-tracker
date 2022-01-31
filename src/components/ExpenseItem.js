f15ddb896051fceccc7fb4bc90fecf0a191a81a8
Andriy Trofimov <webgrandmo@gmail.com>
2022-01-31 20:33:40 +0200

    Add props and create component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ExpenseItem.css";
+function ExpenseItem({ title, date, amount }) {
+  return (
+    <div className="expense-item">
+      <div>{date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
